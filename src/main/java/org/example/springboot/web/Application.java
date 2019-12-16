90c8c44635022efb1aa48818bf3c4abc6ec7c803
Seoungju Oh <osj4532@daum.net>
2019-12-16 18:09:35 +0900

    lombok 적용 및 dto, controller test

--- src/main/java/org/example/springboot/web/Application.java
+++ src/main/java/org/example/springboot/web/Application.java
@@ -1,4 +1,4 @@
-package org.example.springboot;
+package org.example.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
