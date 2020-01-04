7668b132e491e157b49ba5f249c14ecd4aaae1d1
Seoungju Oh <osj4532@daum.net>
2020-01-04 21:00:16 +0900

    deploy.sh 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,14 +7,14 @@ echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 CURRENT_PID=$(pgrep -fl springboot2-webservice | grep jar | awk '{print $1}')
 
-echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -36,4 +36,7 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
+
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
