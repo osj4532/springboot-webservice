01b54113c48dc9d29497a11f24671ae2210c7dc7
Seoungju Oh <osj4532@daum.net>
2020-01-06 16:08:34 +0900

    make ProfileController and test

--- src/main/java/org/example/springboot/config/auth/SecurityConfig.java
+++ src/main/java/org/example/springboot/config/auth/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     //url별 권한 관리를 설정하는 옵션의 시작점
                     .authorizeRequests()
                     //antMatchers에 해당하는 url은 permitAll로 권한 상관없이 모두 허용
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     // /api/v1/** 주소를 가진 API는 User 권한을 가진 사람만 가능
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     //위 설정된 값들을 제외한 나머지 URL은 인증된 사용자들만 허용
